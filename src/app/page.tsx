112691d2d64f4a72779fbae3b55bd6f7bb2f5eae
Tuine Cloud <tuinecloudoriginal@gmail.com>
2025-06-22 22:00:34 +0600

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Tuine Cloud</h1>
-      <p>Your stylish clothing brand.</p>
-    </main>
-  );
-}
